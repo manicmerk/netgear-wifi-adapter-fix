20c0fef552eee9c6a2b0c40a20af7e48be4a05f1
pbemerk <pbemerk@gmail.com>
2020-07-04 12:16:02 -0400

    after discussion with a mentor we were able to successfully solve if else implementation.

--- Program.cs
+++ Program.cs
@@ -14,46 +14,42 @@ namespace Wi_Fi_Drop_Fix
             Console.WriteLine("\n Do you want to toggle AutoConfig setting for your Wi-Fi Adapter?");
             Console.Write("\n Please input On, Off or Quit? ");
 
-            String outcome = Outcome();
+            string outcome = Outcome();
 
             Console.WriteLine("\n " + outcome + "? Press ENTER to continue.");
             Console.ReadLine();
 
-            Toggle();
+            Toggle(outcome);
             Console.ReadLine();
+            Console.Write("Goodbye!");
         }
 
         // Method to return answer for user outcome interface.
         static public string Outcome()
         {
             string outcome = Console.ReadLine();
-            return outcome = outcome.ToUpper();
+            return outcome.ToUpper();
         }
 
         // Toggle Autoconfig setting on or off.
-        static void Toggle()
+        static void Toggle(string outcome)
         {
             string toggleOn = "/c netsh wlan set autoconfig enabled=yes interface=\"Wi-Fi 4\"";
             string toggleOff = "/c netsh wlan set autoconfig enabled=no interface=\"Wi-Fi 4\"";
-            string outcome = Outcome();
 
             if (outcome == "ON")
             {
-                Console.WriteLine("Autoconfig Enabled. You can detect wireless routers now.");
+                Console.WriteLine("You can now detect wireless routers.");
                 System.Diagnostics.Process.Start("CMD.exe", toggleOn);
-                Console.ReadLine();
             }
             else if (outcome == "OFF")
             {
-                Console.WriteLine("Autoconfig Disabled. You are connected to wireless but unable to detect new routers.");
+                Console.WriteLine("You are connected to wireless but unable to detect new routers.");
                 System.Diagnostics.Process.Start("CMD.exe", toggleOff);
-                Console.ReadLine();
             }
             else if (outcome == "QUIT")
             {
                 Console.WriteLine("Goodbye!");
-                Console.Write("Press Enter to Exit ");
-                Console.ReadLine();
                 Environment.Exit(0);
             }
         }
