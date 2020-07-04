7116ab7e85bf566ca63f1589d76dce79cfe09220
pbemerk <pbemerk@gmail.com>
2020-07-04 11:07:53 -0400

    Methods moved out of main. If statement converted to if else. Toggle() still not working to change autoconfig setting or display text?

--- Program.cs
+++ Program.cs
@@ -7,15 +7,12 @@ namespace Wi_Fi_Drop_Fix
         static void Main(string[] args)
         {
             // Initiate CMD prompt to display autoconfig setting causing lag bombs.
-
-
             string destination = "/c netsh wlan show settings";
             System.Diagnostics.Process.Start("CMD.exe", destination);
             Console.ReadLine();
 
             Console.WriteLine("\n Do you want to toggle AutoConfig setting for your Wi-Fi Adapter?");
             Console.Write("\n Please input On, Off or Quit? ");
-
 
             String outcome = Outcome();
 
@@ -26,12 +23,14 @@ namespace Wi_Fi_Drop_Fix
             Console.ReadLine();
         }
 
+        // Method to return answer for user outcome interface.
         static public string Outcome()
         {
             string outcome = Console.ReadLine();
             return outcome = outcome.ToUpper();
         }
 
+        // Toggle Autoconfig setting on or off.
         static void Toggle()
         {
             string toggleOn = "/c netsh wlan set autoconfig enabled=yes interface=\"Wi-Fi 4\"";
@@ -40,11 +39,22 @@ namespace Wi_Fi_Drop_Fix
 
             if (outcome == "ON")
             {
+                Console.WriteLine("Autoconfig Enabled. You can detect wireless routers now.");
                 System.Diagnostics.Process.Start("CMD.exe", toggleOn);
+                Console.ReadLine();
             }
-            else (outcome == "OFF")
+            else if (outcome == "OFF")
             {
+                Console.WriteLine("Autoconfig Disabled. You are connected to wireless but unable to detect new routers.");
                 System.Diagnostics.Process.Start("CMD.exe", toggleOff);
+                Console.ReadLine();
+            }
+            else if (outcome == "QUIT")
+            {
+                Console.WriteLine("Goodbye!");
+                Console.Write("Press Enter to Exit ");
+                Console.ReadLine();
+                Environment.Exit(0);
             }
         }
 
